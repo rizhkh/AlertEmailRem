cd8061902ef6939741cda1e2a15468cb79e56fb3
Riz <mrk_rizwan@hotmail.com>
2018-07-05 13:05:24 -0400

    Add files via upload
    First file for the script that would be executed on the server

---
+++ email.py
@@ -0,0 +1,7 @@
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+
+your_addrs = "rizwanhkhan101@gmail.com" # Later this would be input string of your email addresss
+tothesent_addrs = "mrk150@rutgers.edu"
