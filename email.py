6f52f9d8b6818767078e1ae86fa23a34e6fff1de
Riz <mrk_rizwan@hotmail.com>
2018-07-05 13:07:01 -0400

    Update email.py

--- email.py
+++ email.py
@@ -5,3 +5,9 @@ from email.MIMEText import MIMEText
 
 your_addrs = "YourEmailAddress@gmail.com" # Later this would be input string of your email addresss
 tothesent_addrs = "EmailAddressYouAreSendingThisTo@email.com"
+
+
+msg = MIMEMultipart()
+msg['From'] = your_addrs
+msg['To'] = tothesent_addrs
+msg['Subject'] = "Reminder:"
