ec4820db56c978019e81a398d6703e99f745f7ac
Riz <mrk_rizwan@hotmail.com>
2018-07-05 13:07:36 -0400

    Update email.py

--- email.py
+++ email.py
@@ -2,10 +2,8 @@ import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
-
 your_addrs = "YourEmailAddress@gmail.com" # Later this would be input string of your email addresss
 tothesent_addrs = "EmailAddressYouAreSendingThisTo@email.com"
-
 
 msg = MIMEMultipart()
 msg['From'] = your_addrs
@@ -15,3 +13,9 @@ msg['Subject'] = "Reminder:"
 body = "TESTING EMAIL"
 msg.attach(MIMEText(body, 'plain'))
 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(your_addrs, "PASSWORD")
+text = msg.as_string()
+server.sendmail(your_addrs, tothesent_addrs, text)
+server.quit()
