32f116ef7deff57708f2b05e9fba73f9f50e7e96
Riz <mrk_rizwan@hotmail.com>
2018-07-05 13:15:40 -0400

    Update email.py

--- email.py
+++ email.py
@@ -1,6 +1,6 @@
 import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 your_addrs = "YourEmailAddress@gmail.com" # Later this would be input string of your email addresss
 tothesent_addrs = "EmailAddressYouAreSendingThisTo@email.com"
